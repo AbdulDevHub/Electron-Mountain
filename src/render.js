f85c0e4d00f823caaa53ff07c768dd04d836da05
Abdul Khan <ahkn63@gmail.com>
2024-05-11 20:28:29 -0400

    Allow Audio Recording [Untested]

--- src/render.js
+++ src/render.js
@@ -54,7 +54,12 @@ async function selectSource(source) {
   videoSelectBtn.innerText = source.name;
 
   const constraints = {
-    audio: false,
+    audio: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: source.id,
+      },
+    },
     video: {
       mandatory: {
         chromeMediaSource: 'desktop',
